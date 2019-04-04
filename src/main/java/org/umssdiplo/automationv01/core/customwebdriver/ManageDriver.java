c3d4687763db756504313ba110e242344a1718e9
Tania Valero <tativalero@gmail.com>
2019-04-04 08:12:34 -0400

    Fix configuration for execution of login

--- src/main/java/org/umssdiplo/automationv01/core/customwebdriver/ManageDriver.java
+++ src/main/java/org/umssdiplo/automationv01/core/customwebdriver/ManageDriver.java
@@ -19,6 +19,11 @@ public class ManageDriver {
         BrowserType driverType = BrowserType.valueOf(PropertyAccessor.getInstance().getBrowser());
         webDriver = DriverFactory.getManageDriver(driverType);
         webDriver.manage().window().maximize();
+
+        restorePreviousTimeWait();
+
+//        setExplicitTimeWait(EXPLICIT_TIME_WAIT);
+//        webDriverWait = new WebDriverWait(webDriver, EXPLICIT_TIME_WAIT);
     }
 
     public static ManageDriver getInstance() {
