66de496f81411e99320d63eb8d315f544dba419c
Tania Valero <tativalero@gmail.com>
2019-04-08 07:34:13 -0400

    First configuration, for online store

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
@@ -1,13 +1,12 @@
 package org.umssdiplo.automationv01.stepdefinitionproject;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionPHPtravel {
-    private Login login;
+    private OnlineStore onlineStore;
 
     @Given("^'PHP travel' page is loaded$")
     public void phpTravelPageIsLoaded() throws Throwable {
@@ -17,5 +16,10 @@ public class StepsDefinitionPHPtravel {
     @And("^set my credentials on 'Login' page$")
     public void setMyCredentialsOnLoginPage() throws Throwable {
         login.setCredentials();
+    }
+
+    @And("^click 'category' in 'card'$")
+    public void clickCategoryInCard() {
+        onlineStore.clickShowCategory
     }
 }
---
+++ src/test/resources/features/Category.feature
@@ -0,0 +1,5 @@
+Feature: Funcionalidad para mostrar categoria
+
+  Scenario: Seleccionar una categoria
+    Given 'Tienda Online' page is loaded
+    And click 'category' in 'card'
